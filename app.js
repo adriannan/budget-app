fb49c83ae727acc4af81739577905408d37b4eb4
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2020-03-17 18:50:49 +0100

    destructure DOM-strings

--- app.js
+++ app.js
@@ -6,30 +6,44 @@ const budgetController = (function() {
 
 // UI CONTROLLER
 const UIController = (function() {
+  const DOMStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
+
   return {
     getInput: function() {
       return {
-        type: document.querySelector(".add__type").value,
-        description: document.querySelector(".add__description").value,
-        value: document.querySelector(".add__value").value
+        type: document.querySelector(DOMStrings.inputType).value,
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
       };
+    },
+    getDOMStrings: function() {
+      return DOMStrings;
     }
   };
 })();
 
 // APP CONTROLLER
 const controller = (function(budgetCtrl, UICtrl) {
+  const DOMStrings = UICtrl.getDOMStrings();
   const addItem = () => {
     //  1. get the field input data
     let getInput = UICtrl.getInput();
     console.log(getInput);
+
     //  2. add the item to the budget controller
     //  3. add the item to the UI
     //  4. calculate the budget
     //  5. display the budget on the UI
   };
 
-  document.querySelector(".add__btn").addEventListener("click", addItem);
+  document
+    .querySelector(DOMStrings.inputBtn)
+    .addEventListener("click", addItem);
   document.addEventListener("keypress", function(e) {
     if (e.keyCode === 13 || e.which === 13) {
       addItem();
