376581b0108506fdce515e4b79b404d1334335e6
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2020-03-18 15:19:55 +0100

    add new item to the UI

--- app.js
+++ app.js
@@ -58,7 +58,9 @@ const UIController = (function() {
     inputType: ".add__type",
     inputDescription: ".add__description",
     inputValue: ".add__value",
-    inputBtn: ".add__btn"
+    inputBtn: ".add__btn",
+    incomeList: ".income__list",
+    expenseList: ".expenses__list"
   };
 
   return {
@@ -68,6 +70,40 @@ const UIController = (function() {
         description: document.querySelector(DOMStrings.inputDescription).value,
         value: document.querySelector(DOMStrings.inputValue).value
       };
+    },
+    addListItem: function(obj, type) {
+      let html, list;
+
+      // Create HTML string with placeholder text
+      // replace the placeholder text with some actual data
+
+      if (type === "exp") {
+        list = document.querySelector(DOMStrings.expenseList);
+        html = `<div class="item clearfix" id="expense-${obj.id}">
+                  <div class="item__description">${obj.description}</div>
+                  <div class="right clearfix">
+                    <div class="item__value">${obj.value}</div>
+                    <div class="item__delete">
+                     <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                    </div>
+                  </div>
+                </div>`;
+      } else if (type === "inc") {
+        list = document.querySelector(DOMStrings.incomeList);
+        html = `<div class="item clearfix" id="income-${obj.id}">
+                  <div class="item__description">${obj.description}</div>
+                  <div class="right clearfix">
+                    <div class="item__value">${obj.value}</div>
+                    <div class="item__delete">
+                      <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                    </div>
+                  </div>
+                </div>`;
+      }
+
+      // insert the hTML into the DOM
+
+      list.insertAdjacentHTML("beforeend", html);
     },
     getDOMStrings: function() {
       return DOMStrings;
@@ -90,18 +126,24 @@ const controller = (function(budgetCtrl, UICtrl) {
   };
 
   const addItem = () => {
-    let getInput, newItem;
+    let getInput, newItem, newListItem;
 
     //  1. get the field input data
+
     getInput = UICtrl.getInput();
 
     //  2. add the item to the budget controller
+
     newItem = budgetCtrl.addItem(
       getInput.type,
       getInput.description,
       getInput.value
     );
+
     //  3. add the item to the UI
+
+    newListItem = UICtrl.addListItem(newItem, getInput.type);
+
     //  4. calculate the budget
     //  5. display the budget on the UI
   };
